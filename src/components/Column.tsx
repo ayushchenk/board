551b9313e212eb269588a0727084fda2aebb4053
Oleksandr Yushchenko1 <Oleksandr_Yushchenko1@epam.com>
2022-07-14 16:48:28 +0300

    add styling to column

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -9,17 +9,20 @@ export interface ColumnProps {
 }
 
 export function Column(props: ColumnProps) {
-    const [, dropRef] = useDrop(() => ({
+    const [isHovering, dropRef] = useDrop(() => ({
         accept: "card",
-        drop: (item: any) => props.moveCard(item.code, props.header)
+        drop: (item: any) => props.moveCard(item.code, props.header),
+        collect: (monitor) => monitor.isOver()
     }));
 
     const cards = props.cards
         .sort((a, b) => b.priority - a.priority)
         .map((c, i) => <Card card={c} key={i}></Card>);
 
+    const className = isHovering ? "column column--hovering" : "column";
+
     return (
-        <div ref={dropRef} className="column">
+        <div ref={dropRef} className={className}>
             <h3 className="column__header">{props.header}</h3>
             {cards}
         </div>
